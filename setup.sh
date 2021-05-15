474880dbcc2bf47c0a510df5eee76ac016e15c00
Hagsteel <me@hagsteel.com>
2021-05-15 10:53:33 +0200

    added setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p pack/dev/start
+cd pack/dev/start
+git clone https://github.com/neovim/nvim-lspconfig
+git clone https://github.com/sirver/UltiSnips
+git clone https://github.com/tpope/vim-commentary
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/togglebyte/togglerust
+git clone https://github.com/junegunn/fzf.vim

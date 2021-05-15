7bc06f4fb5f96edf49df22ec88e6ddab76c5ba9e
Hagsteel <me@hagsteel.com>
2021-05-15 20:14:18 +0200

    added a basic setup file and some comments

--- setup.sh
+++ setup.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-mkdir -p pack/dev/start
-cd pack/dev/start
-git clone https://github.com/neovim/nvim-lspconfig
-git clone https://github.com/sirver/UltiSnips
-git clone https://github.com/tpope/vim-commentary
-git clone https://github.com/tpope/vim-surround
-git clone https://github.com/togglebyte/togglerust
-git clone https://github.com/junegunn/fzf.vim
+
+# List of all packages to install.
+# Currently this requires that each package
+# lives on github.com.
+PACKAGES=(
+    "neovim/nvim-lspconfig"
+    "sirver/UltiSnips"
+    "tpope/vim-commentary"
+    "tpope/vim-surround"
+    "togglebyte/togglerust"
+    "junegunn/fzf.vim"
+    "rust-lang/rust.vim"
+)
+
+if [[ $1 == "--init" ]]
+then
+    # Create the pack/dev/start directory 
+    # and clone all the plugins into it
+    mkdir -p pack/dev/start
+    cd pack/dev/start
+    for package in "${PACKAGES[@]}"
+    do
+        git clone "https://github.com/$package"
+    done
+else
+    # Git pull every package in pack/dev/start
+    for package in `ls pack/dev/start`
+    do
+        path="pack/dev/start/$package"
+        (cd $path && git pull)
+    done
+fi

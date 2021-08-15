b67933d4b083baf85a9bec18ef1d0eb4c73a89fa
Hagsteel <me@hagsteel.com>
2021-08-15 17:22:08 +0200

    fixing more silly mistakes

--- init.lua
+++ init.lua
@@ -1,26 +1,26 @@
--- local map = vim.api.nvim_set_keymap
+local map = vim.api.nvim_set_keymap
 
 vim.cmd(':so $HOME/.config/nvim/nolua.vim')
 
 -----------------------------------------------------------------------------
 --    LSP
 ------------------------------------------------------------------------------
--- local nvim_lsp = require'lspconfig'
--- local on_attach = function(client)
---     map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', {})
--- end
+local nvim_lsp = require'lspconfig'
+local on_attach = function(client)
+    map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', {})
+end
 
 
--- nvim_lsp.rust_analyzer.setup({
---     on_attach = on_attach,
--- })
+nvim_lsp.rust_analyzer.setup({
+    on_attach = on_attach,
+})
 
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
--- 	vim.lsp.diagnostic.on_publish_diagnostics, {
--- 		virtual_text = false, 
---         signs = false, 
---         update_in_insert = false,
---         underline = false,
---         severity_sort = false,
--- 	}
--- )
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		virtual_text = false, 
+        signs = false, 
+        update_in_insert = false,
+        underline = false,
+        severity_sort = false,
+	}
+)

6e5a89c3f74e08f930713133544952c1ff311b7c
Togglebit <me@togglebit.io>
2024-11-10 16:47:59 +0100

    updated to use diagnostic config instead of vim lsp

--- init.lua
+++ init.lua
@@ -6,25 +6,21 @@ vim.cmd(':so $HOME/.config/nvim/nolua.vim')
 --    LSP
 -----------------------------------------------------------------------------
 local nvim_lsp = require'lspconfig'
+
 local on_attach = function(client)
     map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', {})
     client.server_capabilities.semanticTokensProvider = nil
 end
 
+
 nvim_lsp.rust_analyzer.setup({
     on_attach = on_attach,
 })
 
-nvim_lsp.tsserver.setup({
-    on_attach = on_attach,
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = false, 
+    update_in_insert = false,
+    underline = false,
+    severity_sort = false,
 })
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false, 
-        signs = false, 
-        update_in_insert = false,
-        underline = false,
-        severity_sort = false,
-    }
-)

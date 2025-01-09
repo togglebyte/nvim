757ebb3c02c4dc19c7c35826a80232a57508a24a
Togglebit <me@togglebit.io>
2025-01-09 17:03:39 +0100

    deref snippet

--- init.lua
+++ init.lua
@@ -15,10 +15,6 @@ nvim_lsp.rust_analyzer.setup({
     on_attach = on_attach,
 })
 
-nvim_lsp.tsserver.setup({
-    on_attach = on_attach,
-})
-
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = false, 
